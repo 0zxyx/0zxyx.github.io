a0a1ae1927d1addaba0473ffab2c790eb9985b9d
0zxyx <67796396+0zxyx@users.noreply.github.com>
2022-09-30 02:21:31 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+ <!DOCTYPE html>
+ <html>
+         <head>  <meta charset="utf-8">
+		 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		 <meta name="description" content="Cheatsheet Search Engine Created For Penetration Testing.">
+		 <meta name="keywords" content="0zxyx, cheatsheet, hackthebox, htb, tryhackme, thm, writeups">
+		 <meta name="author" content="0zxyx">
+                 <link rel="stylesheet" href="/css/styles.css">
+                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+                 <title>0zxyx.com | Writeups</title>
+         </head>
+
+         <body>
+
+
+                 <h1 class="head">Welcome to 0zxyx</h1>
+
+
+                 <div class="nav-menu">
+                         <a href="/" class="nav-link">HOME</a>
+                         <a href="/writeups" class="nav-link">WRITEUPS</a>
+                         <a href="https://twitter.com/messages/compose?recipient_id=1241195067806605314" class="nav-link">CONTACT</a>
+                         <a href="/blog" class="nav-link">BLOG</a>
+                 </div>
+
+		 <a href="https://www.buymeacoffee.com/0zxyx">
+			 <img  class="buymelogo" alt="Buy me a coffee" src="https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg">
+		 </a>
+
+
+                 <div class="icon-list">
+                         <a href="https://github.com/0zxyx" class="github"><i class="fa-brands fa-github github"></i></a> 
+                         <a href="https://twitter.com/0zxyx" class="twitter"><i class="fa-brands fa-twitter"></i></a> 
+                 </div>
+
+                 <div class="wp-menu">
+                         <a  href="/writeups" class="nav-link">Writeups</a>
+                 </div>
+
+
+                <a href="#" class="gotop">
+                <i class="fa-solid fa-arrow-up"></i></a>
+
+                <script src="/js/main.js"></script>
+
+
+         </body>
+ </html> 
+
+?>
