729108d9b2681a32a51437ef7f0f3bfd523fe8a8
0zxyx <67796396+0zxyx@users.noreply.github.com>
2022-10-11 03:45:17 -0400

    Update search.js

--- js/search.js
+++ js/search.js
@@ -2,13 +2,13 @@ $(document).ready(function(){
         $('.search-box').keyup(function(){
 		$('.result').html('');
                 var nsearch = $('.search-box').val();
-		var regex = new RegExp(nsearch, "i");
+		var sanitize = nsearch.replace(/[^a-zA-Z ]/gi, '');
+		var regex = new RegExp(sanitize, "i");
 		if ($.trim(nsearch) != '')
 		$.getJSON('/data/data.json', function(data){
 			$.each(data, function(key, value){
 				if(value.name.search(regex) != -1)
 				{
-					//$('.result').append(value.name + " " +  value.link + "<br>");
 					$('.result').append('<a href=" '+value.link+'" class="result-link">'+value.name+'</a>');
 				}
 
