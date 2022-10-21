9d3afe96fe19daed4e51e07522578249aec44d7d
0zxyx <67796396+0zxyx@users.noreply.github.com>
2022-10-21 01:33:05 -0400

    Update scroll.js

--- js/scroll.js
+++ js/scroll.js
@@ -1,8 +1,18 @@
 $(document).ready(function(){
 	var scroll = $(document).scrollTop();
+	var active=true;
+	$('.menu').click(function(){
+		if(active){
+			$('.nav-menu').fadeOut('fast');
+			active=false;
+		}else{
+			$('.nav-menu').fadeIn('fast');
+			active=true;
+		}
+	});
     $(window).scroll(function(){
-	   var cs = $(document).scrollTop();
-        if (scroll >= cs) {
+	    var cs = $(document).scrollTop();
+        if (scroll >= cs && active == true) {
             $('.nav-menu').fadeIn();
 	}else{
 		$('.nav-menu').fadeOut();
