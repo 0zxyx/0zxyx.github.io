fea064f36221b2f5342719864d29c6539873a492
0zxyx <67796396+0zxyx@users.noreply.github.com>
2023-09-21 08:10:39 -0400

    Update syscalls.js

--- js/syscalls.js
+++ js/syscalls.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
                 var call = $('.syscall-box').val();
 		var arch = $('.arch option:selected').val();
 		var sanicall = call.replace(/[^a-zA-Z ]/gi, '');
+		var sanicall = sanicall.toLowerCase();
 		var saniarch = arch.replace(/[^0-9 ]/gi, '');
 		if ($.trim(sanicall) != '')
 		$.getJSON('/data/syscalls.json', function(data){
